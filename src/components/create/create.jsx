ae8cbd555054a43e9dac5097e919b9279f227564
nourinnnu <165750333+nourinnnu@users.noreply.github.com>
2024-04-28 11:51:23 +0530

    Add files via upload

---
+++ src/components/create/create.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import "./create.css"
+
+
+export const Create = () => {
+  return (
+    <>
+      <section className='newPost'>
+        <div className='container boxItems'>
+          <div className='img '>
+            <img src={'/create.jpg'} alt='' class='image-preview' />
+          </div>
+          <form>
+            <div className='inputfile flexCenter'>
+              <input type='file' accept='image/*' alt='img' />
+            </div>
+            <input type='text' placeholder='Title' />
+
+            <textarea name='' id='' cols='30' rows='10'></textarea>
+
+            <button className='button'>Create Post</button>
+          </form>
+        </div>
+      </section>
+    </>
+  )
+}
