8e8966d2c2a9076549f3a3db44fba77ca338b77b
nourinnnu <165750333+nourinnnu@users.noreply.github.com>
2024-04-28 11:27:27 +0530

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,127 @@
+export const nav = [
+    {
+      id: 1,
+      text: "home",
+      url: "/",
+    },
+    {
+      id: 2,
+      text: "about",
+      url: "/about",
+    },
+    {
+      id: 3,
+      text: "pages",
+      url: "/pages",
+    },
+    {
+      id: 4,
+      text: "blog",
+      url: "/blog",
+    },
+    {
+      id: 5,
+      text: "contact",
+      url: "/contact",
+    },
+  ]
+  export const category = [
+    {
+      id: 1,
+      category: "FERRARI",
+      title: "on wheels",
+      cover: "/ferrari.jpg",
+    },
+    {
+      id: 2,
+      category: "RANGE ROVER",
+      title: "WOOooooWWWwww",
+      cover: "/range.jpg",
+    },
+    {
+      id: 3,
+      category: "AMBASSADOR",
+      title: "Aesthetic",
+      cover: "/ambassor.jpg",
+    },
+    {
+      id: 4,
+      category: "MERCEDES",
+      title: "Stay Calm And Surf",
+      cover: "/mercedes.jpg",
+    },
+    {
+      id: 5,
+      category: "Jeep",
+      title: "Travel Time",
+      cover: "/jeep.jpg",
+    },
+    {
+      id: 6,
+      category: "HYUNDAI",
+      title: "On Roads",
+      cover: "/hyundai.jpg",
+    },
+    
+  ]
+  export const blog = [
+    {
+      id: 1,
+      title: "Always the Aesthetic one",
+      desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare. ",
+      category: "AMBASSADOR",
+      cover: "/amb.jpg",
+      date: "APRIL 05, 2024",
+    },
+  
+    {
+      id: 2,
+      title: "A Luxurios Sports Car",
+      desc:  " Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras porttitor",
+      category: "FERRARI",
+      cover: "/fer.jpg",
+      date: "APRIL 15, 2024",
+    },
+    {
+      id: 3,
+      title: "Through The mountains",
+      desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare. ",
+      category: "Jeep",
+      cover: "/jee.jpg",
+      date: "MARCH 15, 2024",
+    },
+    {
+      id: 4,
+      title: "The Cool and Calm",
+      desc: "Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras porttitor condimentum ci suscipit.",
+      category: "MERCEDES",
+      cover: "/mer.jpg",
+      date: "APRIL 20, 2024",
+    },
+    {
+      id: 5,
+      title: "Minimalistic",
+      desc: "Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras porttitor condimentum orci suscipit.",
+      category: "HYUNDAI",
+      cover: "/hyu.jpg",
+      date: "APRIL 20, 2024",
+    },
+    {
+      id: 6,
+      title: "A range through",
+      desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius",
+      category: "RANGE ROVER",
+      cover: "/ran.jpg",
+      date: "APRIL 20, 2024",
+    },
+    {
+      id: 7,
+      title: "A range through",
+      desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius",
+      category: "FERRARI",
+      cover: "/kkkkk.jpg",
+      date: "APRIL 20, 2024",
+    },
+    
+]
+  
